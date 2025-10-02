9c02a992dc9267e25b1786d55f4c3b38821d4382
ShararOhee <96702162+ShararOhee@users.noreply.github.com>
2025-10-02 00:29:04 -0500

    New Weapon / Attack System

---
+++ Assets/Script/Player/Weapons/Sword.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    [SerializeField] private GameObject slashAnimPrefab;
+    [SerializeField] private Transform slashAnimSpawnPoint;
+
+    private PlayerControls playerControls;
+    private Animator myAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
+
+    private GameObject slashAnim;
+
+    private void Awake()
+    {
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
+        myAnimator = GetComponent<Animator>();
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    void Start()
+    {
+        playerControls.Player.Attack.started += _ => Attack();
+    }
+
+    private void Update()
+    {
+        MouseFollowWithOffset();
+    }
+
+    private void Attack()
+    {
+        myAnimator.SetTrigger("Attack");
+
+    }
+
+    private void MouseFollowWithOffset()
+    {
+        Vector2 mousePos = playerControls.Player.MousePosition.ReadValue<Vector2>();
+        Vector2 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        Vector2 direction = mousePos - playerScreenPoint;
+
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+           
+            activeWeapon.transform.rotation = Quaternion.Euler(180, 0, -angle);
+        }
+        else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+}
