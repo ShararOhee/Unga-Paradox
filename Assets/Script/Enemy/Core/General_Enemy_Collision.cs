1923a7f09642ea9e264c6a7e46376a5ec73ecf62
HuyVoDN <huy.businesscontact@gmail.com>
2025-10-07 15:14:17 -0500

    added some minor bug fixes when enemy collapse into one another

--- Assets/Script/Enemy/Core/General_Enemy_Collision.cs
+++ Assets/Script/Enemy/Core/General_Enemy_Collision.cs
@@ -3,24 +3,31 @@ using UnityEngine;
 public class General_Enemy_Collision : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private General_Enemy_Wandering enemyWandering;
+    private General_Enemy_Wandering wanderingScript;
+    private General_Enemy_Movement movementScript;
 
     void Awake()
     {   // this transform.parent is needed to call the sibling script in the same parent gameObject
-        enemyWandering = transform.parent.GetComponentInChildren<General_Enemy_Wandering>(); // go to the parent, then actually get the component in the parent's children lmao
-        if (enemyWandering == null)
-            Debug.LogError("General_Enemy_Wandering not found on " + gameObject.name);
+        wanderingScript = transform.parent.GetComponentInChildren<General_Enemy_Wandering>(); // go to the parent, then actually get the component in the parent's children lmao
+        movementScript = transform.parent.GetComponentInChildren<General_Enemy_Movement>();
+
+        if (wanderingScript == null) 
+            throw new MissingComponentException("General_Enemy_Detection missing!");
+        if (movementScript == null) 
+            throw new MissingComponentException("General_Enemy_Movement missing!");
+
     }
 
     public void OnParentCollision(Collision2D collision) // needs to update collision detection
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            //Debug.Log(HelperFuncs.GetOwnerName(transform) + " Collided With Character! Stopping and wandering away.");
-            StopAllCoroutines(); // Stop any current wandering coroutine  
-            StartCoroutine(enemyWandering.IdleAndSetNewWanderPoint());
+            Debug.Log(HelperFuncs.GetOwnerName(transform) + " Collided With Character! Stopping and wandering away.");
+            StopAllCoroutines(); // Stop any current wandering coroutine
+            movementScript.Stop();                  // 
+            StartCoroutine(wanderingScript.IdleAndSetNewWanderPoint());
         }
-       
-       
+
+
     }
 }
