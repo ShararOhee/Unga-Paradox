f045e7092188d68510a5c25de5a90492b716c81c
HuyVoDN <huy.businesscontact@gmail.com>
2025-10-07 15:42:02 -0500

    removed movement script referencing

--- Assets/Script/Enemy/Core/General_Enemy_Collision.cs
+++ Assets/Script/Enemy/Core/General_Enemy_Collision.cs
@@ -4,17 +4,14 @@ public class General_Enemy_Collision : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private General_Enemy_Wandering wanderingScript;
-    private General_Enemy_Movement movementScript;
+    
 
     void Awake()
     {   // this transform.parent is needed to call the sibling script in the same parent gameObject
         wanderingScript = transform.parent.GetComponentInChildren<General_Enemy_Wandering>(); // go to the parent, then actually get the component in the parent's children lmao
-        movementScript = transform.parent.GetComponentInChildren<General_Enemy_Movement>();
-
+       
         if (wanderingScript == null) 
-            throw new MissingComponentException("General_Enemy_Detection missing!");
-        if (movementScript == null) 
-            throw new MissingComponentException("General_Enemy_Movement missing!");
+            throw new MissingComponentException("General_Enemy_Detection missing!");       
 
     }
 
@@ -24,7 +21,7 @@ public class General_Enemy_Collision : MonoBehaviour
         {
             Debug.Log(HelperFuncs.GetOwnerName(transform) + " Collided With Character! Stopping and wandering away.");
             StopAllCoroutines(); // Stop any current wandering coroutine
-            movementScript.Stop();                  // 
+            //movementScript.Stop();                  // 
             StartCoroutine(wanderingScript.IdleAndSetNewWanderPoint());
         }
 
